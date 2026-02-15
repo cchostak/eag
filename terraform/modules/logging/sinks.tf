1db2e9409689f1776597fa19f6884e0c4252ee27
chri c <c.chostak@gmail.com>
2026-02-15 17:19:43 +0000

    fix fmt

--- terraform/modules/logging/sinks.tf
+++ terraform/modules/logging/sinks.tf
@@ -4,12 +4,10 @@ resource "google_logging_project_sink" "to_logging_bucket" {
   destination            = "logging.googleapis.com/projects/${var.project_id}/locations/${var.log_bucket_location}/buckets/${google_logging_project_bucket_config.audit.bucket_id}"
   filter                 = "(${local.effective_log_filter}) OR logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\""
   unique_writer_identity = true
-  include_children       = false
 }
 
-resource "google_logging_project_bucket_config_iam_member" "sink_writer" {
+resource "google_logging_project_bucket_iam_member" "sink_writer" {
   project  = var.project_id
-  location = var.log_bucket_location
   bucket   = google_logging_project_bucket_config.audit.bucket_id
   role     = "roles/logging.bucketWriter"
   member   = google_logging_project_sink.to_logging_bucket.writer_identity
@@ -21,7 +19,6 @@ resource "google_logging_project_sink" "to_archive" {
   destination            = "storage.googleapis.com/${google_storage_bucket.archive.name}"
   filter                 = google_logging_project_sink.to_logging_bucket.filter
   unique_writer_identity = true
-  include_children       = false
 }
 
 resource "google_storage_bucket_iam_member" "archive_writer" {
